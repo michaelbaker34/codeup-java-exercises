4ad3da978b6a41b16fad193fbd5ab1b6923ae1f5
Michael Baker <michael.a.baker34@gmail.com>
2021-03-03 23:36:01 -0600

    complete java syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.print("Hello, ");
-        System.out.println("World!");
+        // System.out.print("Hello, ");
+        // System.out.println("World!");
 
-        int myFavoriteNumber = 1;
-        System.out.println("ln 08: " + myFavoriteNumber);
+        // int myFavoriteNumber = 1;
+        // System.out.println("ln 08: " + myFavoriteNumber);
 
-        String myString = "string";
-        System.out.println("ln 11: " + myString);
+        // String myString = "string";
+        // System.out.println("ln 11: " + myString);
 
-        double myNumber = 3.14;
-        myNumber = 1.23F;
-        System.out.println("ln 14: " + myNumber);
+        // double myNumber = 3.14;
+        // myNumber = 1.23F;
+        // System.out.println("ln 14: " + myNumber);
+
+        // int x = 5;
+        // System.out.println(x++);
+        // System.out.println(x);
+
+        // System.out.println(++x);
+        // System.out.println(x);
+
+        // String theNumberThree = "three";
+        // Object o = theNumberThree;
+        // int three = (int) o;
+        // int three = (int) "three";
+
+        // int x = 4 + 5;
+        // System.out.println(x);
+
+        // int y = 3 * 4;
+        // System.out.println(y);
+
+        // int y = 2 - 5;
+        // System.out.println(y);
+
+        // int max = Integer.MAX_VALUE;
+        // int min = Integer.MIN_VALUE;
+        // max++;
+        // min--;
+        // System.out.println(max);
+        // System.out.println(min);
 
     }
 }
