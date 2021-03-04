bd0feec96d611a79c7638155b4e614c1fc1794c1
Michael Baker <michael.a.baker34@gmail.com>
2021-03-04 11:04:37 -0600

    format and print pi variable

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,8 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+        String piFormatted = String.format("Value of pi is approx %.2f", pi);
+        System.out.println(piFormatted);
+    }
+}
