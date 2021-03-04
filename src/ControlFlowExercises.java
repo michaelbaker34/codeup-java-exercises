526da9da3e612b52ed44443fd900b4c9b5b29326
Michael Baker <michael.a.baker34@gmail.com>
2021-03-04 16:57:45 -0600

    refactor infinite 2 squared loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,15 +2,9 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-//        for (int i = 5; i <= 15; i++) {
-//            System.out.println(i);
-//        }
-
-        int i = 2;
-        do {
-            i = i * i;
+        for (int i = 2; i < 1000000; i *= i) {
             System.out.println(i);
-        } while (i < 1000000);
+        }
 
 
 
