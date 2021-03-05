abdcf014337163e3d48d180568531e8ac4c2dcb8
Michael Baker <michael.a.baker34@gmail.com>
2021-03-05 16:57:34 -0600

    add lastchar variable start if loop

--- src/Bob.java
+++ src/Bob.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        String userInput = sc.nextLine();
+        char lastChar = userInput.charAt(0);
+        System.out.println("Talk to Bob:");
+        System.out.println(userInput);
 //      Bob answers 'Sure.' if you ask him a question.
 //        (the input ends with a question mark)
+
+//        if () {
+//
+//        }
 
 //      He answers 'Whoa, chill out!' if you yell at him.
 //        (the input ends with an exclamation mark)
 
+
 //      He says 'Fine. Be that way!' if you address him without saying anything.
 //          (empty input)
+
 
 //      He answers 'Whatever.' to anything else.
 
