544663740fc58bf3f761f8356c31fd3de930f828
Michael Baker <michael.a.baker34@gmail.com>
2021-03-05 09:27:55 -0600

    complete fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,17 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        for (int i = 2; i < 1000000; i *= i) {
-            System.out.println(i);
+        for (int i = 0; i < 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
-
 
 
     }
