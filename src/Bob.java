6ed18c51eb7697f5f85c6f8a69e64188ca6193db
Michael Baker <michael.a.baker34@gmail.com>
2021-03-06 22:21:26 -0600

    add if else loops for responses

--- src/Bob.java
+++ src/Bob.java
@@ -9,22 +9,31 @@ public class Bob {
         char lastChar = userInput.charAt(0);
         System.out.println("Talk to Bob:");
         System.out.println(userInput);
+
 //      Bob answers 'Sure.' if you ask him a question.
 //        (the input ends with a question mark)
 
-//        if () {
-//
-//        }
+        if (lastChar == '.') {
+            System.out.println("Sure.");
+        }
 
 //      He answers 'Whoa, chill out!' if you yell at him.
 //        (the input ends with an exclamation mark)
 
+        if (lastChar == '!') {
+            System.out.println("Whoa, chill out!");
+        }
 
 //      He says 'Fine. Be that way!' if you address him without saying anything.
 //          (empty input)
-
-
 //      He answers 'Whatever.' to anything else.
+
+
+        if (userInput.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
 
 
     }
