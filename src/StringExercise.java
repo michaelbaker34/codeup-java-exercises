ee0410a2b4421fa975a3fd949289525675ab2d9d
Michael Baker <michael.a.baker34@gmail.com>
2021-03-08 11:16:31 -0600

    refactor logs

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,13 +1,16 @@
 public class StringExercise {
     public static void main(String[] args) {
 
-
-        System.out.println("We don't need no education\n" +
-                "We don't need no thought control");
-        System.out.println("Check \"this\" out!, \"s inside of \"s!");
-        System.out.println("In windows, the main drive is usually C:\\");
-        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" +
-                "and the amazing triple backslash \\\\\\!");
+        String out1 = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(out1);
+        String out2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(out2);
+        String out3 = "In windows, the main drive is usually C:\\";
+        System.out.println(out3);
+        String out4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(out4);
 
     }
 }
