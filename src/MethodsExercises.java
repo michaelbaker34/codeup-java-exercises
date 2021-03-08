2bd75eb4a4a4b4a47274a36def3b57037d180fb6
Michael Baker <michael.a.baker34@gmail.com>
2021-03-08 15:14:00 -0600

    complete recursion refactor

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -7,8 +7,12 @@ public class MethodsExercises {
 //        add();
 //        subtract();
 //        multiply();
+//        System.out.println(multiplyLoop(3, 5));
+//        System.out.println(multiplyRecursion(3, 5));
 //        divide();
-//        mod();
+//        mod(3, 2);
+//        getInteger(1, 10);
+//        getFactorial();
 
     }
 
@@ -39,6 +43,24 @@ public class MethodsExercises {
         System.out.printf("%d * %d = " + product + "\n", input1, input2);
     }
 
+    public static int multiplyLoop(int x, int y) {
+        int result = 0;
+        while (x > 0) {
+            result += y;
+            x--;
+        }
+        System.out.println("3 * 5 = " + result);
+        return (result);
+    }
+
+    public static int multiplyRecursion(int num1, int num2) {
+        int multiplied = 0;
+        if (num2 != 0) {
+            multiplied = num1 + multiplyRecursion(num1, num2 - 1);
+        }
+        return multiplied;
+    }
+
     public static void divide() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers to divide:");
@@ -48,15 +70,31 @@ public class MethodsExercises {
         System.out.printf("%d / %d = " + quotient + "\n", input1, input2);
     }
 
-//    public static void mod() {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter two numbers to find the remainder:");
-//        int input1 = sc.nextInt();
-//        int input2 = sc.nextInt();
-//        int mod = input1 % input2;
-//        System.out.printf("%d % %d = " + mod + "\n", input1, input2);
-//    }
+    public static int mod(int num1, int num2) {
+        int mod = num1 % num2;
+        System.out.println(num1 + " % " + num2 + " = " + mod);
+        return mod;
+    }
 
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10:");
+        int userInput = sc.nextInt();
+        if (userInput >= min && userInput <= max) {
+            System.out.println("Number is: " + userInput);
+            return userInput;
+        } else {
+            System.out.println("Number out of range \n");
+            return getInteger(1, 10);
+        }
+    }
+
+//    public static int getFactorial() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number between 1 and 10:");
+//        int userInput = sc.nextInt();
+//
+//    }
 
 
 }
