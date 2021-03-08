301c222c32eb4c4994331e4c0a9301254aaa1b82
Michael Baker <michael.a.baker34@gmail.com>
2021-03-08 11:27:01 -0600

    complete and refactor bob.java

--- src/Bob.java
+++ src/Bob.java
@@ -4,36 +4,32 @@ public class Bob {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        String userInput = sc.nextLine();
-        char lastChar = userInput.charAt(0);
         System.out.println("Talk to Bob:");
-        System.out.println(userInput);
+        String userInput = sc.nextLine();
 
 //      Bob answers 'Sure.' if you ask him a question.
 //        (the input ends with a question mark)
-
-        if (lastChar == '.') {
-            System.out.println("Sure.");
-        }
-
 //      He answers 'Whoa, chill out!' if you yell at him.
 //        (the input ends with an exclamation mark)
-
-        if (lastChar == '!') {
-            System.out.println("Whoa, chill out!");
-        }
-
 //      He says 'Fine. Be that way!' if you address him without saying anything.
 //          (empty input)
 //      He answers 'Whatever.' to anything else.
 
-
-        if (userInput.isEmpty()) {
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Woah, Chill out!");
+        } else if (userInput.isEmpty()) {
             System.out.println("Fine. Be that way!");
         } else {
-            System.out.println("Whatever.");
+            System.out.println("Whatever");
         }
+
+
+
+
+
+
 
 
     }
