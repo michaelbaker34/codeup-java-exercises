ba91b31353e077787763daff3fdf754222223d6d
Michael Baker <michael.a.baker34@gmail.com>
2021-03-10 19:54:28 -0600

    complete references exercise

--- src/Person.java
+++ src/Person.java
@@ -21,7 +21,27 @@ public class Person {
     public static void main(String[] args) {
 
         Person p1 = new Person("bob");
-        System.out.println(p1.name);
+//        System.out.println(p1.name);
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.name);
+//        System.out.println(person2.name);
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+
 
     }
 
