98fa63d5ac392088eec88d865f8dada391c47297
Michael Baker <michael.a.baker34@gmail.com>
2021-03-12 12:32:01 -0600

    complete server name generator exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+
+    public static String[] adjectives = {"adj1", "adj2", "adj3"};
+    public static String[] nouns      = {"noun1", "noun2", "noun3"};
+
+    public static String getWord(String[] words) {
+        Random random = new Random();
+        int randomNum = random.nextInt(words.length);
+        return words[randomNum];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getWord(adjectives) + "-" + getWord(nouns));
+
+    }
+
+}
