67f6f1c6f8dae3c80b0daf5a114b9a4426df75cb
Michael Baker <michael.a.baker34@gmail.com>
2021-03-12 12:31:31 -0600

    refactor and comment

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,15 +3,18 @@ import java.util.Arrays;
 public class ArraysExercises {
 
     public static void main(String[] args) {
-
+        // create people array with 3 indexes
         Person[] people = new Person[3];
 
+        // assign each index to a string
         people[0] = new Person("bob");
         people[1] = new Person("dan");
         people[2] = new Person("jon");
 
+        // use addPerson method to add a new index and string
         people = addPerson(people, new Person("jan"));
 
+        // print name of the person string at each index of the people array
         for (Person person : people) {
             System.out.println(person.getName());
         }
@@ -19,8 +22,13 @@ public class ArraysExercises {
     }
 
     public static Person[] addPerson(Person[] people, Person person) {
+
+        // assign newPeople to a copy of people array with another index
         Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+
+        // set the last index of newPeople array to person parameter
         newPeople[newPeople.length - 1] = person;
+
         return newPeople;
     }
 
